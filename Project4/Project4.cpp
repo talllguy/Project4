8282747422a25be9b8ecbf35375a3f0829c3fad1
talllguy <elliott.plack@gmail.com>
2013-11-03 16:50:18 -0500

    Got a test working.

--- Project4/Project4.cpp
+++ Project4/Project4.cpp
@@ -33,15 +33,16 @@ int main()
 	inFile >> init1 >> init2 >> init3 >> posCode >> age >> dollarsPerHour >> payCode; // read input from file
 
 	//test to output on screen first
-	
 	cout << "Name" << setw(12) << "Position" << setw(12) << "Age" << setw(12) << "Annual" << setw(12) << "Pay Code\n";
+	cout << init1 << init2 << init3 << setw(12) << posCode << setw(12) << age << setw(12) << dollarsPerHour << setw(12) << payCode << endl;
 	
-	cout << "There should be " << fileLines << " lines.\n"; // test of the lines counter
+	
+	//cout << "There should be " << fileLines << " lines.\n"; // test of the lines counter
 
-	for (int i = 0; i < fileLines; i++)
-	{
-		cout << init1 << init2 << init3 << setw(12) << posCode << setw(12) << age << setw(12) << dollarsPerHour << setw(12) << payCode << endl;
-	}
+	//for (int i = 0; i < fileLines; i++)
+	//{
+	//	cout << init1 << init2 << init3 << setw(12) << posCode << setw(12) << age << setw(12) << dollarsPerHour << setw(12) << payCode << endl;
+	//}
 	return 0;
 }
 
