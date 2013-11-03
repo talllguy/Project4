439577ea8f61c1c03e0ed8212dca54dda8bc2bd3
talllguy <elliott.plack@gmail.com>
2013-11-03 16:40:04 -0500

    first test run. Hit it!

--- Project4/Project4.cpp
+++ Project4/Project4.cpp
@@ -8,6 +8,7 @@ Algorithm:	Read infile of employees into variables. Process certain
 
 #include <fstream>
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 void posType (char); // function that will convert the posCode to a position name
@@ -17,18 +18,27 @@ int Code (int);      // function that will convert code to pay code
   
 int main() 
 { 
-    char init1('X'),init2('X'),init3('X'); // the three initials
+    int fileLines(0); // number of lines in file
+	char init1('X'),init2('X'),init3('X'); // the three initials
 	char posCode('X'); // position of worker
 	int age(0); // age of worker
 	float dollarsPerHour(0.); // salary of employee (per hour)
 	int payCode(0); //pay code
 
 	ifstream inFile;     // define ifstream to inFile command
-	inFile.open("employees.txt");
+	
+	fileLines = count(istreambuf_iterator<char>(inFile),istreambuf_iterator<char>(), '\n');
 
+	inFile.open("Employees.txt");
 	inFile >> init1 >> init2 >> init3 >> posCode >> age >> dollarsPerHour >> payCode; // read input from file
 
+	//test to output on screen first
 
-
+	cout << "Name" << setw(12) << "Position" << setw(12) << "Age" << setw(12) << "Annual" << setw(12) << "Pay Code";
+	for (int i = 0; i < fileLines; i++)
+	{
+		cout << init1 << init2 << init3 << setw(12) << posCode << setw(12) << age << setw(12) << dollarsPerHour << setw(12) << payCode;
+	}
+	return 0;
 }
 
