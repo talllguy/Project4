b60d3d7d75b076eae997bfbffa8bae72b1f42ec2
talllguy <elliott.plack@gmail.com>
2013-11-03 16:55:13 -0500

    Did another test. Noticed there needs to be a new line at the end of file.

--- Project4/Project4.cpp
+++ Project4/Project4.cpp
@@ -26,18 +26,17 @@ int main()
 	int payCode(0); //pay code
 
 	ifstream inFile;     // define ifstream to inFile command
-	
-	fileLines = count(istreambuf_iterator<char>(inFile),istreambuf_iterator<char>(), '\n');
 
-	inFile.open("Employees.txt");
+	inFile.open("Employees.txt"); // first open to count lines
+	fileLines = count(istreambuf_iterator<char>(inFile),istreambuf_iterator<char>(), '\n'); // count lines
+	inFile.close(); // then close
+	inFile.open("Employees.txt"); // open again
 	inFile >> init1 >> init2 >> init3 >> posCode >> age >> dollarsPerHour >> payCode; // read input from file
 
 	//test to output on screen first
 	cout << "Name" << setw(12) << "Position" << setw(12) << "Age" << setw(12) << "Annual" << setw(12) << "Pay Code\n";
 	cout << init1 << init2 << init3 << setw(12) << posCode << setw(12) << age << setw(12) << dollarsPerHour << setw(12) << payCode << endl;
-	
-	
-	//cout << "There should be " << fileLines << " lines.\n"; // test of the lines counter
+	cout << "There should be " << fileLines << " lines.\n"; // test of the lines counter
 
 	//for (int i = 0; i < fileLines; i++)
 	//{
