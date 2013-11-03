a71a1e78de2aed2ac0b951a01dd37197b1d9a7e7
talllguy <elliott.plack@gmail.com>
2013-11-03 16:47:07 -0500

    Lets try this again with a file

--- Project4/Project4.cpp
+++ Project4/Project4.cpp
@@ -34,13 +34,13 @@ int main()
 
 	//test to output on screen first
 	
-	cout << "Name" << setw(12) << "Position" << setw(12) << "Age" << setw(12) << "Annual" << setw(12) << "Pay Code";
+	cout << "Name" << setw(12) << "Position" << setw(12) << "Age" << setw(12) << "Annual" << setw(12) << "Pay Code\n";
 	
-	cout << "There should be " << fileLines << " lines."; // test of the lines counter
+	cout << "There should be " << fileLines << " lines.\n"; // test of the lines counter
 
 	for (int i = 0; i < fileLines; i++)
 	{
-		cout << init1 << init2 << init3 << setw(12) << posCode << setw(12) << age << setw(12) << dollarsPerHour << setw(12) << payCode;
+		cout << init1 << init2 << init3 << setw(12) << posCode << setw(12) << age << setw(12) << dollarsPerHour << setw(12) << payCode << endl;
 	}
 	return 0;
 }
